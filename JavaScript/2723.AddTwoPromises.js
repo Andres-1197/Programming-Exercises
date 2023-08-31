77f9983e3a53a13c6ed380df7809c1aa92b9f02c
Andres <andresfernandez@gmail.com>
2023-08-31 14:29:26 +1200

    new javascript problems

---
+++ JavaScript/2723.AddTwoPromises.js
@@ -0,0 +1,13 @@
+// PROBLEM
+
+//Given two promises promise1 and promise2, return a new promise. 
+//promise1 and promise2 will both resolve with a number. 
+//The returned promise should resolve with the sum of the two numbers.
+
+var addTwoPromises = async function(promise1, promise2) {
+    const [result1, result2] = await Promise.all([promise1, promise2]);
+    return result1 + result2;
+};
+
+addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+  .then(console.log); 
