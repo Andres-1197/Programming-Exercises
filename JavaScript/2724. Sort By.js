0ac2002ed738ac2d8c9929a9052a4b01e5a6c827
Andres <andresfernandez@gmail.com>
2023-12-31 14:36:02 +1300

    update and new problem added

---
+++ JavaScript/2724. Sort By.js
@@ -0,0 +1,17 @@
+// PROBLEM
+
+//Given an array arr and a function fn, return a sorted array sortedArr. You can assume fn only returns numbers and those numbers determine the sort order of sortedArr. sortedArray must be sorted in ascending order by fn output.
+
+//You may assume that fn will never duplicate numbers for a given array.
+
+/**
+ * @param {Array} arr
+ * @param {Function} fn
+ * @return {Array}
+ */
+
+var sortBy = function(arr, fn) {
+    return arr.slice().sort(function(a, b) {
+        return fn(a) - fn(b);
+    });
+}
